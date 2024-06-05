fa833b7ec5e31a87b37907109923a7c7437416d4
Yubeol <113155432+Yubeol@users.noreply.github.com>
2024-06-05 15:31:22 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,18 +1,24 @@
-package com.ki.level0.p120807;
+package com.ki.level0.p120829;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(2, 3)); // -1
-        System.out.println(new Solution().solution(11, 11)); // 1
-        System.out.println(new Solution().solution(7, 99)); // -1
-    }
-}
 
-class Solution {
-    public int solution(int num1, int num2) {
-        if (num1 == num2) {
-            return 1;
+
         }
-        return -1;
+    }
+
+    class Solution {
+        public int solution(int angle) {
+            if (angle > 0 && 90 < angle) {
+                answer = 1;
+            } else if (angle == 90) {
+                answer = 2;
+            } else if (angle > 90 && angle < 180) {
+                answer = 3;
+            } else {
+                answer = 4;
+            }
+            return answer;
+
     }
 }
